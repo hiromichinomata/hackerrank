1a4f62eb96495f876ce74a0a5120a7a336a11372
Hiromichi NOMATA <git@hiromichinomata.com>
2020-05-16 22:44:44 +0900

    [add] 30 days of code

---
+++ 30_days_of_code/001_data_types.rb
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+i2 = gets.to_i
+d2 = gets.to_f
+s2 = gets
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+puts i + i2
+# Print the sum of the double variables on a new line.
+puts d + d2
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + s2
