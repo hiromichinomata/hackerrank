a4b612b6b4fae7f1b27bdae3dc2537c5d27783ce
Hiromichi NOMATA <git@hiromichinomata.com>
2020-06-07 09:47:15 +0900

    [add] day 24

---
+++ 30_days_of_code/024_more_linked_lists.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        cur =head
+        nums = set()
+        while cur.next is not None:
+            nex = cur.next
+            nums.add(cur.data)
+            if nex.data in nums:
+                cur.next = nex.next
+            else:
+                cur = nex
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head);
