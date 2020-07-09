c63f7ab02062baa77d2bea2e8cbb831ff8481fd4
Hiromichi NOMATA <git@hiromichinomata.com>
2020-07-09 20:38:17 +0900

    [fix] python exceptions

--- python/errors_and_exceptions/exceptions.py
+++ python/errors_and_exceptions/exceptions.py
@@ -2,7 +2,7 @@ num = int(input().strip())
 for _ in range(num):
     try:
         a, b = list(map(int, input().strip().split()))
-        result = a/b
+        result = a//b
         print(result)
-    except Error as e:
+    except Exception as e:
         print("Error Code:",e)
