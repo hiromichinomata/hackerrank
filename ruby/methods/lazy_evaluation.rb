392e76ba52a6a3e953fdc55d8f1358af6a311cbb
Hiromichi NOMATA <git@hiromichinomata.com>
2020-08-08 10:37:08 +0900

    [add] use ruby lazy

--- ruby/methods/lazy_evaluation.rb
+++ ruby/methods/lazy_evaluation.rb
@@ -1,17 +1,8 @@
 require 'prime'
 
 n = gets.chomp.to_i
-count = 0
-i = 1
-result = []
-while count < n do
-    i += 1
-    if Prime.prime?(i)
-        si = i.to_s
-        if si == si.reverse
-            count += 1
-            result.push(i)
-        end
-    end
-end
+result = Prime.each.lazy.filter{ |i|
+  s_i = i.to_s
+  s_i == s_i.reverse
+}.first(n)
 p result
